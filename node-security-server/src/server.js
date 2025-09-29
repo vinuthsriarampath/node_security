c88ec7484f286f4d91f795806cb03614662de819
vinuth <vinuthsriarampath@outlook.com>
2025-09-29 21:25:08 +0530

    feat: connect to mongo database

--- node-security-server/src/server.js
+++ node-security-server/src/server.js
@@ -1,9 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app.js';
+import mongoose from 'mongoose';
 
 const PORT = process.env.PORT;
 
+// CONECTING TO MONGO DATABSE 
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB connected successfully!"))
+    .catch(err => console.error(err));
+
+// STARTS THE SERVER
 app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}/`)
+    console.log(`ServerS running on http://localhost:${PORT}/`)
 })
