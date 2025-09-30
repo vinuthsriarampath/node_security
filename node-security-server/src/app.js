a06922924700db6adf4f143ade60060bfbcd4b16
vinuth <vinuthsriarampath@outlook.com>
2025-09-30 18:46:07 +0530

    feat: add cors origin to the server

--- node-security-server/src/app.js
+++ node-security-server/src/app.js
@@ -1,9 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import indexRoute from './routes/index-route.js';
+import cors from 'cors';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+console.log(process.env.FRONTEND_URL)
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    credentials: true,
+}))
 
 app.use('/api',indexRoute);
 
--- node-security-server/src/server.js
+++ node-security-server/src/server.js
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import app from './app.js';
 import mongoose from 'mongoose';
 
