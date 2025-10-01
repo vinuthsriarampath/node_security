69a3f234ed1b2b8ecfd76692ff9cdf53b52ad2b3
vinuth <vinuthsriarampath@outlook.com>
2025-10-01 23:14:45 +0530

    feat: add express-rate-limit middleware to enhance rate limiting for login and registration routes

--- node-security-server/src/routes/auth-route.js
+++ node-security-server/src/routes/auth-route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { login, register, refreshToken, logout } from '../controllers/auth-controller.js';
+import { loginRateLimit, registerRateLimit } from '../middlewares/ratelimit-middleware.js';
 
 const router =  express.Router();
 
-router.post('/register',register);
-router.post('/login',login);
+router.post('/register',registerRateLimit,register);
+router.post('/login',loginRateLimit,login);
 router.post('/refresh',refreshToken);
 router.post('/logout',logout);
 
